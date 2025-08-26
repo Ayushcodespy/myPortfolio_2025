00abdf672862871481fc5709fac32acc5d561541
Ayushcodespy <ayush.jnv54@gmail.com>
2025-08-27 01:09:47 +0530

    STATICFILES_DIRS = [BASE_DIR,'static'] is added

--- myPortfolio_2025/settings.py
+++ myPortfolio_2025/settings.py
@@ -122,6 +122,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # where collectstatic will collect all f
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR,"static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
